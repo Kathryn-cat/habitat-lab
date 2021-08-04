90a574c2f2e70dcaa9f2cdc8e96d5d1c50913c49
Kathryn-cat <jinqic@andrew.cmu.edu>
2021-08-04 17:47:10 -0400

    changed object info template

--- new_configs/utils/objects.py
+++ new_configs/utils/objects.py
@@ -1,4 +1,5 @@
 import habitat_sim
+from tabulate import tabulate
 
 def print_object_info(sim):
     mgr = sim.get_rigid_object_manager()
@@ -34,16 +35,15 @@ def print_object_info(sim):
         scenenode_rotation.append(obj.rotation)
 
     print(f'NUMBER OF OBJECTS IN THE ENVIRONMENT: {num_objects}')
+    data = []
     for i in range(num_objects):
-        print(f'\
-            index: {i}\t\
-            object id: {ids[i]}\t\
-            handle: {handles[i]}\t\
-            motion type: {motion_types[i]}\t\
-            collidable: {collidable[i]}\t\
-            local collision mesh: {local_bounding_box[i]}\t\
-            global com: {com[i]}\t\
-            translation wrt SceneNode: {scenenode_translation[i]}\t\
-            rotation wrt SceneNode: {scenenode_rotation[i]}\
-        ')
+        data_i = [\
+            i, ids[i], handles[i], motion_types[i], collidable[i], local_bounding_box[i], \
+            com[i], scenenode_translation[i], scenenode_rotation[i]\
+        ]
+        data.append(data_i)
+        headers = ["index", "object id", "handle", "motion type", "collidable", \
+                   "local collision mesh", "global com", "translation w.r.t SceneNode", \
+                   "rotation w.r.t SceneNode"]
+        print(tabulate(data, headers=headers))
 
